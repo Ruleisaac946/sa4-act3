91d3b5d98d012476936c4b02c0bec9cfb758ec84
Isaac Albonetti <ramat946@gmail.com>
2024-02-08 03:03:41 -0600

    Now lets you try until correct

---
+++ guess_number.py
@@ -0,0 +1,17 @@
+number = 10
+
+print("I'm thinking of a number...")
+while True:
+    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+    if guess == 'q':
+        print(f"The number was {number}.")
+        break
+    try:
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        else:
+            print("Sorry! Try again.")
+    except ValueError:
+        print("Please enter a valid number or 'q' to quit.")
