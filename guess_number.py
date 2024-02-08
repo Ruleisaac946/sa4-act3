9d3285c3b16fe519bcfca48bc144cbd7e40ddfc9
Isaac Albonetti <ramat946@gmail.com>
2024-02-08 03:13:40 -0600

    Modified implementation to cap the total number of guesses and provide remaining guesses count

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,10 @@
 number = 10
+max_guesses = 5  
+remaining_guesses = max_guesses  
 
 print("I'm thinking of a number...")
-while True:
-    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+while remaining_guesses > 0:
+    guess = input(f"What number am I thinking of? You have {remaining_guesses} guesses left. (Enter 'q' to quit) ")
     if guess == 'q':
         print(f"The number was {number}.")
         break
@@ -11,7 +13,13 @@ while True:
         if guess == number:
             print("Congratulations! You guessed the right number.")
             break
+        elif guess < number:
+            print("Sorry! Your guess was too low.")
         else:
-            print("Sorry! Try again.")
+            print("Sorry! Your guess was too high.")
+        remaining_guesses -= 1  # Decrement remaining guesses
+        if remaining_guesses == 0:
+            print(f"Out of guesses! The number was {number}.")
+            break
     except ValueError:
         print("Please enter a valid number or 'q' to quit.")
